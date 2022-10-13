ee3d5d24a3efe4c7798cde9400de898ad982d79c
Jonas Birmé <jonas.birme@eyevinn.se>
2022-10-13 14:18:20 -0700

    fix: remove console log in tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -7,7 +7,7 @@ describe("HLS Multivariant manifest", () => {
     const hls = new DecoratedHls(manifest, { sessionID: "7d623fe5-20cb-45fc-9d0b-57e9fce17611" });
     const m3u = await hls.decorate();
     const arr = m3u.toString().split("\n");
-    console.log(arr);
+    //console.log(arr);
     expect(arr[4]).toEqual("manifest_1.m3u8?CMCD=br%3D4255%2Cot%3Dm%2Csid%3D%227d623fe5-20cb-45fc-9d0b-57e9fce17611%22&type=asdf");
     expect(arr[18]).toEqual('#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID="audio",LANGUAGE="en",NAME="English stereo",CHANNELS="2",DEFAULT=NO,AUTOSELECT=YES,URI="manifest_audio_en.m3u8?CMCD=br%3D862%2Cot%3Dm%2Csid%3D%227d623fe5-20cb-45fc-9d0b-57e9fce17611%22&type=asdf"');
   });
@@ -19,7 +19,7 @@ describe("HLS Media playlist", () => {
     const hls = new DecoratedHls(manifest, { sessionID: "7d623fe5-20cb-45fc-9d0b-57e9fce17611" });
     const m3u = await hls.decorate();
     const arr = m3u.toString().split("\n");
-    console.log(arr);
+    //console.log(arr);
     expect(arr[6]).toEqual("manifest_1_00001.ts?CMCD=d%3D10000%2Csid%3D%227d623fe5-20cb-45fc-9d0b-57e9fce17611%22&type=asdf");
   });
 });
